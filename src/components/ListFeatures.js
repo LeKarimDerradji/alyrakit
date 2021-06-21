6bdbee22bea5e6c8cc24a19f9fa3bfa544f98a05
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-06-21 18:05:31 +0200

    cat

--- src/components/ListFeatures.js
+++ src/components/ListFeatures.js
@@ -1,4 +1,17 @@
 import working from "../assets/working.svg"
+import {
+  Image,
+  Heading,
+  Container,
+  Stack,
+  Center,
+  SimpleGrid,
+  List,
+  ListItem,
+  ListIcon, 
+  Box,
+} from "@chakra-ui/react"
+import { CheckCircleIcon } from "@chakra-ui/icons"
 
 const ListFeatures = () => {
   const list = [
@@ -8,21 +21,38 @@ const ListFeatures = () => {
     "Integration ready",
   ]
   return (
-    <div>
-      <img
+    <Container 
+    bg='gray.50' 
+    maxW='100%' 
+    mb={6} 
+    pt="28" 
+    pb="18">
+      <Center>
+      <Stack direction={["column", null, "row"]} sx={{gap: '2rem'}} cv={3}>
+        <Box>
+      <Image
         src={working}
         alt="Illustration with a computer on the desk"
         width="400"
         height="295"
       />
-
-      <h2>The most useful resource ever created for designers</h2>
-      <ul>
+      </Box>
+      <Box>
+      <Heading fontSize='4xl'  maxW="30ch" mt={9} mb={7}>The most useful resource ever created for designers</Heading>
+      <List fontSize='1xl'>
+        <SimpleGrid columns={2}>
         {list.map((el, index) => {
-          return <li key={index}>{el}</li>
+          return <ListItem key={index}> 
+          <ListIcon as={CheckCircleIcon} color="teal.300"/>
+          {el} 
+          </ListItem>
         })}
-      </ul>
-    </div>
+        </SimpleGrid>
+      </List>
+      </Box>
+    </Stack>
+    </Center>
+    </Container>
   )
 }
 
